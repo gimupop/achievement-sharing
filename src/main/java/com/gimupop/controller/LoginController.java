8c1d5c2a4578ababe5ffb97ddde4b5572dc7a503
yanagawa_keita@microad.co.jp <yanagawa_keita@microad.co.jp>
2017-03-17 22:09:07 +0900

    first commit

---
+++ src/main/java/com/gimupop/controller/LoginController.java
@@ -0,0 +1,13 @@
+package com.gimupop.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class LoginController {
+    @RequestMapping("/login")
+    public String login() {
+        return "html/login.html";
+    }
+}
